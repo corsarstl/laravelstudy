418ce362d0520f5638c907d94f3fab32f54852e8
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-10-20 13:39:30 +0300

    controllers

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,8 @@
 |
 */
 
-Route::get('/tasks', function () {
-//    $tasks = DB::table('tasks')->get();
-    $tasks = App\Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-//    $task = DB::table('tasks')->find($id);
-    $task = App\Task::find($id);
-    App\Task::incomplete();
-
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/about', function () {
 
