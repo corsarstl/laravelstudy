eb5070dd2db031f67186ac5c8238e780ea1b3e19
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-10-20 20:13:23 +0300

    Form Validation 101

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -24,18 +24,29 @@ class PostsController extends Controller
 
     public function store()
     {
+//        ***variant 1***
 //        $post = new Post;
-/*      // create a new post using the request data
-        $post->title = request('title');
-        $post->body = request('body');
+//
+//        create a new post using the request data
+//        $post->title = request('title');
+//        $post->body = request('body');
+//
+//        // save it to the database
+//        $post->save();
+//
+//        ***variant 2***
+//        Post::create([
+//            'title' => request('title'),
+//            'body'  => request('body')
+//        ]);
 
-        // save it to the database
-        $post->save();*/
-
-        Post::create([
-            'title' => request('title'),
-            'body'  => request('body')
+//        data validation
+        $this->validate(request(), [
+            'title' => 'required',
+            'body'  => 'required'
         ]);
+
+        Post::create(request(['title', 'body']));
 
         // redirect to the home page
         return redirect('/');
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="col-sm-8 blog-main">
+
         <h1>Publish a Post</h1>
 
         <hr>
@@ -12,15 +13,22 @@
 
             <div class="form-group">
                 <label for="title">Title:</label>
-                <input type="text" id="title" name="title" aria-describedby="titleHelp" placeholder="Enter the title" class="form-control">
+                <input type="text" id="title" name="title" aria-describedby="titleHelp"
+                       placeholder="Enter the title" class="form-control">
             </div>
 
             <div class="form-group">
                 <label for="body">Body</label>
-                <textarea id="body" name="body" cols="30" rows="10" class="form-control"></textarea>
+                <textarea id="body" name="body" cols="30" rows="3" class="form-control"></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary">Publish</button>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Publish</button>
+            </div>
+
+            @include('layouts.errors')
+
         </form>
+
     </div>
 @endsection
